24530440d5750c5f517c44728fe4537415184385
gonzo-li <gonzo_li@chtsecurity.com>
2024-05-28 20:23:08 +0800

    * Create Pokemon project.

---
+++ Pokemon/HomeView.swift
@@ -0,0 +1,24 @@
+//
+//  HomeView.swift
+//  Pokemon
+//
+//  Created by gonzo_li on 2024/5/28.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by gonzo_li on 2024/5/28.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
